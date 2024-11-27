f2d7ffa5ce830dd5553c20b5354d6615515a02f4
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2024-11-27 14:25:40 -0300

    login funcionando e redirecionando

--- scriptLogin.js
+++ scriptLogin.js
@@ -7,7 +7,7 @@
 
 const url = 'http://localhost:8081/login'
 
-document.getElementById('loginUsuarioform').addEventListener('submit', function (event) {
+document.getElementById('botaoLogar').addEventListener('click', function () {
     event.preventDefault;
 
     //coleta os dados do formulário
@@ -16,7 +16,8 @@ document.getElementById('loginUsuarioform').addEventListener('submit', function 
 
     //requisição post para a API
 
-    fetch(url, {
+    fetch(url, 
+    {
         method: 'POST',
         headers: {
             'Content-type': 'application/json',
@@ -24,18 +25,33 @@ document.getElementById('loginUsuarioform').addEventListener('submit', function 
         
         body: JSON.stringify({"email":nomeUser, "senha":senha}), // converte dados para json
     })
-
     .then(response => {
-        if(response.ok){
-            return response.text(); //converte a resposta em texto
-        } else {
-            throw new Error('Credenciais Inválidas') 
+        if (!response.ok) {
+            return response.json().then(err => {
+                console.log('Erro:', err);
+                throw new Error(err.message || 'Erro desconhecido');
+            });
         }
+        return response.json(); 
     })
 
     .then(data => {
-        document.getElementById('mensagem').textContent = data;
-        document.getElementById('mensagem').style.color = 'green';
+        console.log("data", data)
+        if (data.ok){
+            window.location.href = 'paginaUsuarios.html';
+        }
+        else{
+            document.getElementById('mensagem').textContent = data.message;
+
+        }
+
+        // if(data.ok){
+        //     return response.text(); //converte a resposta em texto
+        // } else {
+        //     throw new Error('Credenciais Inválidas') 
+        // }
+        // document.getElementById('mensagem').textContent = data;
+        // document.getElementById('mensagem').style.color = 'green';
 
     })
 
